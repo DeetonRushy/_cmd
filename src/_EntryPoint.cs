379c207d273342beb8bf4ce1f929864d6c91ba80
Deeton Rushton <76126281+DeetonRushy@users.noreply.github.com>
2021-01-05 20:08:58 +0000

    added more command line options.

--- src/_EntryPoint.cs
+++ src/_EntryPoint.cs
@@ -10,10 +10,17 @@ namespace _cmd
     {
         static void Main(string[] args)
         {
+            bool c_Exit = false;
             Parser arg_worker = new Parser(args);
 
             arg_worker
-                .add_optional("--disable-plugins", ref PluginLoader.allow_plugins, false);
+                .add_optional("--disable-plugins", ref PluginLoader.allow_plugins, false)
+                .add_optional("--cancel-init", ref c_Exit, true);
+
+            if (c_Exit)
+            {
+                Environment.Exit(0);
+            }
 
             Start _main = new Start();
             _main._CrtMain_Start();
