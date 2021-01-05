8ca32e3ffd10d4213b6de257f9b746f4ddbddc3f
Deeton Rushton <76126281+DeetonRushy@users.noreply.github.com>
2021-01-05 20:09:59 +0000

    Update _EntryPoint.cs

--- src/_EntryPoint.cs
+++ src/_EntryPoint.cs
@@ -16,6 +16,8 @@ namespace _cmd
             arg_worker
                 .add_optional("--disable-plugins", ref PluginLoader.allow_plugins, false)
                 .add_optional("--cancel-init", ref c_Exit, true);
+            
+            // What is the point of --cancel-init? It's for debugging & will be removed once start-up issues are fixed.
 
             if (c_Exit)
             {
